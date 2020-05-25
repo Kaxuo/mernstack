359c76aed5969f99a797ea107a4d92a764441690
Your Name <you@example.com>
2020-05-25 23:52:19 +0200

    make it better

--- client/src/actions/itemActions.js
+++ client/src/actions/itemActions.js
@@ -7,7 +7,7 @@ export const getItems = () =>  dispatch => {
     // this return is going to reducer and get action.type , sending the types, it's going to return all the items 
     dispatch(setItemsLoading())
     // thanks to proxy in package.json we can just add this , not needing localhost
-    axios.get('http://localhost:5000/api/items').then(res => dispatch({
+    axios.get('api/items').then(res => dispatch({
         type:GET_ITEMS,
         payload:res.data
     }))
@@ -22,7 +22,7 @@ export const getItems = () =>  dispatch => {
 
 export const addItem = item => async dispatch => {
     // dont forget to add the item as a second parameter
-    axios.post('http://localhost:5000/api/items', item).then(res => dispatch({
+    axios.post('api/items', item).then(res => dispatch({
         type: ADD_ITEM,
         // res data can be seen in item.js in api , in the backend
         payload:res.data
@@ -31,7 +31,7 @@ export const addItem = item => async dispatch => {
 
 export const deleteItem = id => async dispatch => {
     // delete needs an id , so we have to send a payload to the reducer
-    axios.delete(`http://localhost:5000/api/items/${id}`).then(res => dispatch({
+    axios.delete(`/api/items/${id}`).then(res => dispatch({
         type: DELETE_ITEM,
         // res data can be seen in item.js in api , in the backend
         payload:id
